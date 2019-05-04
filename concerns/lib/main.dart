21f849fdf9af14f63e147f6d0076a89a94de32f3
Bilal <three6tdegree@gmail.com>
2019-05-04 13:26:35 +0500

    refactor for scalability

--- concerns/lib/main.dart
+++ concerns/lib/main.dart
@@ -1,137 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import './designs.dart';
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-//DATA
-class Data {
-  String mStr1;
-  String mStr2;
-}
-
-String category;
-DocumentSnapshot document;
-Firestore instance;
-
-void performUpdate(String cat, String desc, String amount) {
-  //push data to cloud
-  //Update balance
-  //Add record in 'expences-records'
-  instance.runTransaction((Transaction transaction) async {
-    CollectionReference reference = instance.collection('expences-records');
-    int newBal = document['balance'] - int.parse(amount);
-    await document.reference.updateData({'balance': newBal});
-    await reference.add({
-      "amount": amount,
-      "what": desc,
-      "category": cat,
-      "when": Timestamp.now()
-    });
-  });
-}
-
-class MyInputForm extends StatefulWidget {
-  void setInputs(
-      String category0, DocumentSnapshot document0, Firestore instance0) {
-    category = category0;
-    document = document0;
-    instance = instance0;
-  }
-
-  @override
-  _MyInputFormState createState() => new _MyInputFormState();
-}
-
-class _MyInputFormState extends State<MyInputForm> {
-  TextEditingController textEditingController1 = new TextEditingController();
-  TextEditingController textEditingController2 = new TextEditingController();
-  // textEditingController1.text = '0';
-
-  @override
-  Widget build(BuildContext context) {
-    TextField txtAmount = TextField(
-      keyboardType: TextInputType.number,
-      controller: textEditingController1,
-    );
-    TextField txtDesc = TextField(
-      keyboardType: TextInputType.text,
-      controller: textEditingController2,
-    );
-
-    return AlertDialog(
-      title: Text('Enter Expence Details'),
-      content: SingleChildScrollView(
-        child: ListBody(
-          children: <Widget>[
-            Text('Please Enter the Amount Spent in ' + category.toUpperCase()),
-            txtAmount,
-            Text('\n\nDescription (Optional)'),
-            txtDesc,
-          ],
-        ),
-      ),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('<VIEW>'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          //  _showDetailsDialog(context);
-          },
-        ),
-        FlatButton(
-          child: Text('Cancel'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        FlatButton(
-          child: Text('ADD'),
-          onPressed: () {
-            Navigator.of(context).pop();
-            performUpdate(category, textEditingController2.text,
-                textEditingController1.text);
-          },
-        ),
-      ],
-    );
-  }
-}
-
-  /*
-  return showDialog<void>(
-                context: context,
-                barrierDismissible: false, // user must tap button!AlertDialog(
-              return AlertDialog(
-              title: Text('VIEW'),
-              content: StreamBuilder(
-                stream: Firestore.instance.collection('expences-records').snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) return const Text('Loading Details');
-                  return ListView.builder(
-                    itemCount: snapshot.data.documents.length,
-                    itemBuilder: (context, index) => ListTile(
-                      title: Text("index"),
-                    ),
-                  );
-                },
-              ),
-            );*/
-
-
-//DIALOGS
-Future<void> _inputExpenceDialog(BuildContext context, String category,
-    DocumentSnapshot document, Firestore instance) async {
-  return showDialog<void>(
-    context: context,
-    barrierDismissible: false, // user must tap button!
-    builder: (BuildContext context) {
-      MyInputForm a = new MyInputForm();
-      a.setInputs(category, document, instance);
-      return a;
-    },
-  );
-}
+import 'data/index.dart';
+import 'util/index.dart';
+import 'screens/TheInputForm/TheInputForm.dart';
 
 Future<void> _adminDialog(BuildContext context, String category,
     DocumentSnapshot document, Firestore instance) async {
@@ -216,19 +87,28 @@ class Designs {
       //HANDLER
       onTap: () {
         print("onTap");
-        if (type==1) _inputExpenceDialog(context, c.mStr1, document, instance);
+        if (type == 1)
+          showInputForm(context, c.mStr1, document, instance);
       },
       onLongPress: () {
         print("onLongPress");
-        if (type==1) _adminDialog(context, c.mStr2, document, instance);
+        if (type == 1) _adminDialog(context, c.mStr2, document, instance);
       },
     );
   }
 }
 
+int viewMode = 1;
+MyApp app1;
+
+void setViewMode(int mode) {
+  viewMode = mode;
+}
+
 void main() {
   //   MapView.setApiKey("AIzaSyBRRVZQb0kK9_Tvy3u_D84zPSJbZ0RbjNo");
-  runApp(new MyApp());
+  app1 = new MyApp();
+  runApp(app1);
 }
 
 class MyApp extends StatelessWidget {
@@ -286,38 +166,41 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Widget _buildListItems(
-      BuildContext context, DocumentSnapshot document, Firestore instance, int type) {
+  Widget _buildListItems(BuildContext context, DocumentSnapshot document,
+      Firestore instance, int type) {
     //Get Data from Cloud
     Data data = new Data();
 
-    if (type==1) {
+    if (type == 1) {
       data.mStr1 = document['name'].toString();
       data.mStr2 = document['balance'].toString();
     }
 
-    if (type==2) {
-      data.mStr1 = document['when'].toString();
-      data.mStr2 = document['desc'].toString();
+    if (type == 2) {
+      data.mStr1 = padIt(document['category'].toString(), 20) +
+          padIt(document['amount'].toString(), 10) +
+          "     " +
+          document['what'].toString();
+      data.mStr2 = readTimestamp(document['when']);
     }
 
     //UI
     return Designs().getListBox(context, data, document, instance, type);
   }
 
-StreamBuilder listFromDb(String collectionName, int type) {
-  return StreamBuilder(
-    stream: Firestore.instance.collection(collectionName).snapshots(),
+  StreamBuilder listFromDb(String collectionName, int type) {
+    return StreamBuilder(
+        stream: Firestore.instance.collection(collectionName).snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) return const Text('Loading');
           return ListView.builder(
             itemExtent: 80.0,
             itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) => _buildListItems(context, snapshot.data.documents[index], Firestore.instance, type),
+            itemBuilder: (context, index) => _buildListItems(context,
+                snapshot.data.documents[index], Firestore.instance, type),
           ); //ListViewBuilder
-        }
-  );
-}
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -327,18 +210,41 @@ StreamBuilder listFromDb(String collectionName, int type) {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: listFromDb('expences', 1),//listFromDb('expences-records', 2),
-      // floatingActionButton: FloatingActionButton(
-      //  onPressed: _incrementCounter,
-      //  tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    if (viewMode == 1)
+      return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: listFromDb('expences', 1), //listFromDb('expences-records', 2),
+        // floatingActionButton: FloatingActionButton(
+        //  onPressed: _incrementCounter,
+        //  tooltip: 'Increment',
+        //   child: Icon(Icons.add),
+        // ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+
+    if (viewMode == 2)
+      return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: FlatButton(
+            child: Text('<< BACK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              setViewMode(1);
+            },
+          ),
+        ),
+        body: listFromDb('expences-records', 2),
+      );
+
+    // floatingActionButton: FloatingActionButton(
+    //  onPressed: _incrementCounter,
+    //  tooltip: 'Increment',
+    //   child: Icon(Icons.add),listFromDb('expences-records', 2),
+    // ), // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
